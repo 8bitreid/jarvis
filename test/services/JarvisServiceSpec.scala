5b8f0643a2cf8561cdefeaed6e894e6f8a86df2d
Reid Mewborne <rmewborne@me.com>
2019-06-07 00:01:06 -0500

    funspec test

--- test/services/JarvisServiceSpec.scala
+++ test/services/JarvisServiceSpec.scala
@@ -2,25 +2,30 @@ package services
 
 import models.{JarvisResponse, Version}
 import org.scalamock.scalatest.MockFactory
-import org.scalatest.FlatSpec
+import org.scalatest.FunSpec
+import org.scalatest.BeforeAndAfter
 
 import scala.concurrent.Future
 import scala.concurrent.Future.successful
 
-class JarvisServiceSpec extends FlatSpec with MockFactory {
+class JarvisServiceSpec extends FunSpec with MockFactory with BeforeAndAfter{
 
   val expectedResponse: Future[JarvisResponse] = successful(JarvisResponse("All good here. Was there anything else?"))
   val expectedVersion: Future[Version] = successful(Version(1,0,0))
 
-  val mockCollaborator: JarvisService = mock[JarvisService]
+  val jarvisService: JarvisService = mock[JarvisService]
 
-  (mockCollaborator.serverStatus _).expects().returning(successful(JarvisResponse("All good here. Was there anything else?")))
-  (mockCollaborator.getVersion _).expects().returning(successful(Version(1,0,0)))
-
-  "JarvisService" should
-    "reply with a greeting" in {
-    assert(mockCollaborator.serverStatus.value === expectedResponse.value)
-    assert(mockCollaborator.getVersion.value === expectedVersion.value)
+  describe ("serverStatus") {
+    it("should respond with a good status.") {
+      (jarvisService.serverStatus _).expects().returning(successful(JarvisResponse("All good here. Was there anything else?")))
+      assert(jarvisService.serverStatus.value === expectedResponse.value)
+    }
+  }
+  describe("getVersoin"){
+    it("should respond with a valid version"){
+      (jarvisService.getVersion _).expects().returning(successful(Version(1,0,0)))
+      assert(jarvisService.getVersion.value === expectedVersion.value)
+    }
   }
 }
 
